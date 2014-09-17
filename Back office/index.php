3ab9809d46f93cf187b3f7a33406fd970780a376
Marjorie <marjorie.petitpre@gmail.com>
2014-09-17 14:36:19 +0200

    Debut back office
    Signed-off-by: Marjorie <marjorie.petitpre@gmail.com>

---
+++ Back office/index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+	<header>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="user-scalable=no, initial-scale = 1, minimum-scale = 1, maximum-scale = 1, width=device-width">       
+	<link rel="stylesheet" media="screen" href="style_bo.css" type="text/css"/>  
+	<link rel="stylesheet" media="screen" href="bootstrap/css/bootstrap.css" type="text/css"/>
+	<link rel="stylesheet" media="screen" href="bootstrap/css/bootstrap.min.css" type="text/css"/>
+	<link rel="stylesheet" media="screen" href="bootstrap/css/bootstrap-theme.css" type="text/css"/>
+	<link rel="stylesheet" media="screen" href="bootstrap/css/bootstrap-theme.min.css" type="text/css"/>     
+    <title>GrindHouse Leather - Administration</title> 
+    
+    </header>
+
+	<body class="bg">
+		<div class="banner">GrindHouse Leather - Administration</div>
+		<?php include_once 'menu_bo.php'; ?>
+		<h1 class="title_bo">Bienvenue dans l'interface d'administration de GrinHouse Leather</h1>
+		<div class="bo_content">
+			Hinc ille commotus ut iniusta perferens et indigna praefecti custodiam protectoribus mandaverat fidis. quo conperto Montius tunc quaestor acer quidem sed ad lenitatem propensior, consulens in commune advocatos palatinarum primos scholarum adlocutus est mollius docens nec decere haec fieri nec prodesse addensque vocis obiurgatorio sonu quod si id placeret, post statuas Constantii deiectas super adimenda vita praefecto conveniet securius cogitari.
+		</div>
+		<div class="bo_content">
+			Hinc ille commotus ut iniusta perferens et indigna praefecti custodiam protectoribus mandaverat fidis. quo conperto Montius tunc quaestor acer quidem sed ad lenitatem propensior, consulens in commune advocatos palatinarum primos scholarum adlocutus est mollius docens nec decere haec fieri nec prodesse addensque vocis obiurgatorio sonu quod si id placeret, post statuas Constantii deiectas super adimenda vita praefecto conveniet securius cogitari.
+		</div>    
+		
+
+	</body>
+
+	<script type="text/javascript" src="script/jquery-1.10.2.min.js"></script>
+	<script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
+	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+
+
+
+</html>
---
+++ Back office/menu_bo.php
@@ -0,0 +1,46 @@
+<div id="width_sidenav">
+		<div class="panel-group" id="accordion" >
+			  <div class="panel panel-default" >
+			    <div class="panel-heading">
+			      <h4 class="panel-title">
+			        <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" class="accordion-toggle" >
+			          Admin 1
+			        </a>
+			      </h4>
+			    </div>
+			    <div id="collapseOne" class="panel-collapse collapse">
+			      <div class="panel-body">
+			        Test 1
+			      </div>
+			    </div>
+			  </div>
+			  <div class="panel panel-default" >
+			    <div class="panel-heading">
+			      <h4 class="panel-title">
+			        <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+			          Admin 2
+			        </a>
+			      </h4>
+			    </div>
+			    <div id="collapseTwo" class="panel-collapse collapse">
+			      <div class="panel-body">
+			        Test 2
+			      </div>
+			    </div>
+			  </div>
+			  <div class="panel panel-default">
+			    <div class="panel-heading">
+			      <h4 class="panel-title">
+			        <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+			          Admin 3
+			        </a>
+			      </h4>
+			    </div>
+			    <div id="collapseThree" class="panel-collapse collapse">
+			      <div class="panel-body">
+			        Test 3
+			      </div>
+			    </div>
+  			</div>
+		</div>
+		</div>
